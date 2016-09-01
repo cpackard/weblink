7c11c72a48731b4fb5f0a93f414e6e59b9b7bc78
Christian Packard <cpackard@users.noreply.github.com>
2016-08-31 21:43:13 -0400

    Include project description and updated license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject weblink "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A project for finding a path of links between any two URL's."
+  :url "https://github.com/cpackard/weblink/"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-tagsoup/clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]
                  [org.clojure/math.numeric-tower "0.0.4"]
